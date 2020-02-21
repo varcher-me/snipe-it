1c9c5a20cc7fa86114f9f1e64ed79b4c14c5a7e1
varcher <varcher@126.com>
2020-02-22 00:25:18 +0800

    优化入库单打印异常反馈逻辑

--- app/Http/Controllers/BulkAssetsController.php
+++ app/Http/Controllers/BulkAssetsController.php
@@ -275,15 +275,10 @@ class BulkAssetsController extends Controller
         $company = Company::find($request->input('company_id'));
         $assets = Asset::find($request->input("ids"));
 
-        \Session::flash('success', trans('admin/hardware/message.create.success'));
-
-//        return response()->json(['redirect_url' => route('hardware.index')]);
-        return response()->view("hardware/storeform-print", [
-            "form_tag" => $request->input("form_tag"),
-            'purchase_date' => $request->input("purchase_date"),
-            'assets' => $assets,
-            'company'=> $company
-            ],200);
-
+        return view("hardware/storeform-print")
+                ->with("form_tag" , $request->input("form_tag"))
+                ->with('purchase_date' , $request->input("purchase_date"))
+                ->with( 'assets' , $assets)
+                ->with('company', $company);
     }
 }
